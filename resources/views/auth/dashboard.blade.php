00b5dc9c1edbbb05d85de9f187366c2cc1e3e3d4
Ahmet Sezer <2110206036@ogrenci.karabuk.edu.tr>
2024-06-12 14:49:11 +0300

    Update 12.6.24

---
+++ resources/views/auth/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+  
+@section('title', 'Dashboard - Laravel Admin Panel')
+  
+@section('contents')
+  <div class="row">
+    <img  src="{{asset('admin.png')}}" alt="">
+  </div>
+@endsection
---
+++ resources/views/layouts/profile.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title','Edit Product')
+
+@section('contents')
+<h1 class="mb-0">Edit Product</h1>
+<hr>
+
+@endsection
